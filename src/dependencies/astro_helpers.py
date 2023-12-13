4f82f3acdfb20baba248538cf34001579221a577
Alex Pizarro <87215633+RazzMatazz5874@users.noreply.github.com>
2023-12-13 04:16:00 -0600

    Updated astro_helpers.py
    Added documentations for the functions

--- src/dependencies/astro_helpers.py
+++ src/dependencies/astro_helpers.py
@@ -86,7 +86,42 @@ def get_sign_degrees(ecliptic_longitude):
     raise ValueError("Invalid ecliptic longitude")
 
 
+def is_day_chart(sun_longitude, ascendant_longitude):
+    """
+    Determines if an astrological chart is a day chart or a night chart based on the Sun's position relative to the Ascendant. A chart is considered a day chart if the Sun is above the horizon, which means its ecliptic longitude is between the Ascendant's longitude and the Descendant's longitude
+
+    Parameters:
+    - sun_longitude (float): The ecliptic longitude of the Sun, measured in degrees (0-359.99).
+                             This value represents the Sun's position in the zodiac circle.
+
+    - ascendant_longitude (float): The ecliptic longitude of the Ascendant, measured in degrees (0-359.99).
+                                 The Ascendant represents the eastern horizon at the time and place of birth.
+
+    Returns:
+    - bool: True if the chart is a day chart (Sun above the horizon), False if the chart is a night chart (Sun below the horizon).
+    """
+    # Calculate Descendant's longitude
+    descendant_longitude = (ascendant_longitude + 180) % 360
+
+    # Check if Sun is between Ascendant and Descendant
+    if ascendant_longitude < descendant_longitude:
+        return ascendant_longitude <= sun_longitude < descendant_longitude
+    else:
+        return sun_longitude < descendant_longitude or sun_longitude >= ascendant_longitude
+
+
 def is_planet_in_its_traditional_domicile(planet, planet_sign):
+    """
+    Determines if a planet is in its domicile, using the traditional rulership system. This function checks whether a given planet is currently in one of its ruling signs.
+
+    Parameters:
+    - planet (str): The name of the planet (e.g., 'Sun', 'Moon', 'Mercury', etc.).
+    - planet_sign (str): The zodiac sign that the planet is currently in (e.g., 'Aries', 'Taurus', etc.).
+
+    Returns:
+    bool: True if the planet is in its traditional domicile, False otherwise.
+    """
+
     planet_rulerships = {
         'Sun': 'Leo',
         'Moon': 'Cancer',
@@ -107,6 +142,16 @@ def is_planet_in_its_traditional_domicile(planet, planet_sign):
 
 
 def is_planet_in_its_traditional_exaltation(planet, planet_sign):
+    """
+    This function checks if a given planet is currently in its exaltation sign, using the traditional system.
+
+    Parameters:
+    - planet (str): The name of the planet (e.g., 'Sun', 'Moon', 'Mercury', etc.).
+    - planet_sign (str): The zodiac sign that the planet is currently in (e.g., 'Aries', 'Taurus', etc.).
+
+    Returns:
+    bool: True if the planet is in its traditional exaltation sign, False otherwise.
+    """
     planet_exaltations = {
         'Sun': 'Aries',
         'Moon': 'Taurus',
@@ -122,6 +167,19 @@ def is_planet_in_its_traditional_exaltation(planet, planet_sign):
 
 
 def is_planet_super_exalted(planet, planet_sign, planet_degree):
+    """
+    Determines if a planet is super exalted based on its sign and degree.
+
+    Super exaltation is an astrological concept where a planet is not only in its exaltation sign but also in a specific degree within that sign, considered to be a position of utmost strength. This function checks if a given planet is in such a position.
+
+    Parameters:
+    - planet (str): The name of the planet (e.g., 'Sun', 'Moon', 'Mercury', etc.).
+    - planet_sign (str): The zodiac sign that the planet is currently in.
+    - planet_degree (float): The degree of the planet within its current sign. Fractional degrees are truncated to the nearest whole number for this calculation.
+
+    Returns:
+    - bool: True if the planet is super exalted (in both the correct sign and degree), False otherwise.
+    """
     # Dictionary mapping planets to their exaltation sign and specific super exaltation degree
     planet_super_exaltations = {
         'Sun': ('Aries', 19),
@@ -143,10 +201,58 @@ def is_planet_super_exalted(planet, planet_sign, planet_degree):
     else:
         return False
 
-# def is_planet_in_its_detriment
+
+def is_planet_in_its_triplicity(planet, sign, day_or_night_chart):
+    """
+    This function checks if a given planet is in the sign that it has triplicity rulership over under the given condition of it being in a day chart or night chart. 
+
+    Parameters:
+    - planet (str): The name of the planet (e.g., 'Sun', 'Moon', 'Mercury', etc.).
+    - sign (str): The zodiac sign to check for triplicity rulership (e.g., 'Aries', 'Taurus', etc.).
+    - day_or_night_chart (bool): A boolean indicating the type of chart. True for a day chart, False for a night chart.
+
+    Returns:
+    - bool: True if the planet is the triplicity ruler of the sign under the given day/night condition, False otherwise.
+
+    Note:
+    """
+
+    triplicity_rulers = {
+        'Aries': ('Sun', 'Jupiter'),
+        'Taurus': ('Venus', 'Moon'),
+        'Gemini': ('Saturn', 'Mercury'),
+        'Cancer': ('Mars', 'Mars'),
+        'Leo': ('Sun', 'Jupiter'),
+        'Virgo': ('Venus', 'Moon'),
+        'Libra': ('Saturn', 'Mercury'),
+        'Scorpio': ('Mars', 'Mars'),
+        'Sagittarius': ('Sun', 'Jupiter'),
+        'Capricorn': ('Venus', 'Moon'),
+        'Aquarius': ('Saturn', 'Mercury'),
+        'Pisces': ('Mars', 'Mars'),
+    }
+
+    # Determine the appropriate ruler based on whether it's a day or night chart
+    ruler = triplicity_rulers[sign][0] if day_or_night_chart else triplicity_rulers[sign][1]
+
+    return planet == ruler
 
 
 def get_ptolemaic_bound_ruler(sign, degree):
+    """
+    This function determines the ruler of the bound that a planet is in based on the provided sign and degree. This function uses the Ptolemaic system for bound definitions
+
+    Parameters:
+    - sign (str): The zodiac sign (e.g., 'Aries', 'Taurus', etc.).
+    - degree (int): The degree within the sign (0-29).
+
+    Returns:
+    - str: The name of the planet that rules the bound for the given sign and degree. 
+         Returns "Invalid sign or degree" if the sign is not found or if the degree is out of the specified bounds.
+
+    Note:
+    The bounds are defined based on the Ptolemaic system and vary for each sign. 
+    """
     # Dictionary structure for Aries bounds. You'll need to extend this to other signs.
     bounds = {
         'Aries': [
@@ -248,7 +354,21 @@ def get_ptolemaic_bound_ruler(sign, degree):
 
 
 def get_traditional_decan(sign, sign_degrees):
-    # Decans dictionary (Chaldean order)
+    """
+    This function identifies the ruling planet of the decan for in which a planet is currently in, given the sign in question and the sign degrees the planet is in.
+
+    Parameters:
+    - sign (str): The zodiac sign (e.g., 'Aries', 'Taurus', etc.).
+    - sign_degrees (int): The degree within the sign (0-29).
+
+    Returns:
+    - str: The name of the planet ruling the decan of the given sign and degree.
+         Returns None if the sign is not recognized or if the degree is out of bounds.
+
+    Note:
+    The decans are assigned based on the Chaldean order.
+    """
+
     decans = {
         'Aries': [('Mars', 0, 10), ('Sun', 10, 20), ('Venus', 20, 30)],
         'Taurus': [('Mercury', 0, 10), ('Moon', 10, 20), ('Saturn', 20, 30)],
